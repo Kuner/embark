b10c9aca703b52958ee809b2e8d7098cd0b3c877
emizzle <ericmastro@status.im>
2019-02-25 12:02:43 +1100

    fix(@embark/cockpit): Fix decode transaction error
    Reproduce:
    1. Go to cockpit > transactions
    2. Click a transaction
    3. There will be a flicker of an error, then the decoded tx displays OK. Inspecting the network requests, there is a 500 error with a response of
    ```
    AssertionError [ERR_ASSERTION]: invalid remainder
    ```
    This error is also printed in the console.
    
    The issue is that the transaction is not a raw transaction, so instead of trying to decode the non-raw transaction, the transaction can be decoded by web3.

--- packages/embark/src/lib/modules/blockchain_connector/index.js
+++ packages/embark/src/lib/modules/blockchain_connector/index.js
@@ -399,7 +399,7 @@ class BlockchainConnector {
       'get',
       '/embark-api/blockchain/transactions/:hash',
       (req, res) => {
-        self.getTransactionByRawTransactionHash(req.params.hash, (err, transaction) => {
+        self.getTransactionByHash(req.params.hash, (err, transaction) => {
           if (err) {
             self.logger.error(err);
           }
